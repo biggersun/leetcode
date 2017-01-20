4d0a09dbe33e5c5085c013bacdbfc48d284f6c5a
biggersun <2659528347@qq.com>
2017-01-20 10:55:46 +0800

    reset

--- nodejsTest/checkMyCode/checkMyCode.js
+++
@@ -1,6 +0,0 @@
-var fs = require('fs');
-var path = require('path');
-var dir = path.dirname('./node_modules/.bin/jscount');
-console.log(path);
-// var res = fs.readFileSync('.js', 'utf-8');
-// console.log(res.split('\n').length-1);
